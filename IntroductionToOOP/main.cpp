1dcbb381f936586590b126894246687992b6ac3c
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-01-19 21:21:24 +0300

    Constructors

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+//#define DISTACE_CHECK
+
 class Point   // Создавая структуру или класс, мы создаем новый тип данных.
 {// Классы и струткры еще называют пользовательскими или составными типами данных.
 	double x;
@@ -22,7 +24,24 @@ public:
 	{
 		this->y = y;
 	}
+	// Constructor:
+	Point()
+	{
+		x = y = 0;
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
 	// Methods:
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:" << this << endl;
+	}
 	double Distance(Point other)
 	{
 		double xDist = this->x - other.x;
@@ -30,7 +49,25 @@ public:
 		double dist = sqrt(xDist * xDist + yDist * yDist);
 		return dist;
 	}
+	void Print()const
+	{
+		cout << "\tX = " << x << "\tY = " << y << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	
 };
+double Distance(Point A, Point B)
+{
+	double xDist = A.GetX() - B.GetX();
+	double yDist = A.GetY() - B.GetY();
+	double dist = sqrt(xDist * xDist + yDist * yDist);
+	return dist;
+}
 
 void main()
 {
@@ -45,10 +82,27 @@ void main()
 	Point* pA = &A;
 	cout << pA->x << '\t' << pA->y << endl;
 	*/
+#ifdef DISTACE_CHECK
 	Point A;
-	A.SetX(2);A.SetY(3);
+	A.SetX(2); A.SetY(3);
 	Point B;
-	B.SetX(7);B.SetY(8);
+	B.SetX(7); B.SetY(8);
+	Point C;
+	C.SetX(10); C.SetY(12);
 	cout << A.GetX() << "\t" << A.GetY() << "\t" << endl;
 	cout << "The disnance between A and B: " << A.Distance(B) << endl;
+	cout << "The disnance between A and C: " << A.Distance(C) << endl;
+	cout << "The disnance between B and C: " << B.Distance(C) << endl;
+	cout << "The disnance between B and C: " << Distance(A, B) << endl;
+#endif // DISTACE_CHECK
+
+	Point A; // Default constructor
+	//cout << A.GetX() << "\t" << A.GetY() << endl;
+	A.Print();
+
+	Point B(2, 3);
+	B.Print();
+
+	Point C = 4; // Single-Argument constructor
+	C.Print();
 }
