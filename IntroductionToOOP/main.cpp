bad95bb6c0db20ffab21625b0885b0ec705b79ad
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-01-23 21:23:57 +0300

    Constructors

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 
-//#define DISTACE_CHECK
+constexpr auto delimiter = "_______________________________________________________________\n";
+
+//#define DISTANCE_CHECK
+//#define CONSTRUCTOR_CHECK
+//#define ASSIGNMENT_CHECK_1
+#define ASSIGNMENT_CHECK_2
+
 
 class Point   // Создавая структуру или класс, мы создаем новый тип данных.
 {// Классы и струткры еще называют пользовательскими или составными типами данных.
@@ -32,9 +38,24 @@ public:
 		cout << "Constructor:\t" << this << endl;
 	}
 	*/
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:" << this << endl;
+	}
 	~Point()
 	{
 		cout << "Destructor:\t" << this << endl;
+	}
+
+	// Operators:
+	Point operator = (const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 	// Methods:
@@ -46,7 +67,7 @@ public:
 		cout << "1ArgConstructor:" << this << endl;
 	}
 	*/
-	double Distance(Point other)
+	double Distance(const Point& other)
 	{
 		double xDist = this->x - other.x;
 		double yDist = this->y - other.y;
@@ -57,15 +78,15 @@ public:
 	{
 		cout << "\tX = " << x << "\tY = " << y << endl;
 	}
-	Point(double x =0, double y = 0)
+	Point(double x = 0, double y = 0)
 	{
 		this->x = x;
 		this->y = y;
 		cout << "Constructor:\t" << this << endl;
 	}
-	
+
 };
-double Distance(Point A, Point B)
+double Distance(const Point& A, const Point& B)
 {
 	double xDist = A.GetX() - B.GetX();
 	double yDist = A.GetY() - B.GetY();
@@ -86,7 +107,7 @@ void main()
 	Point* pA = &A;
 	cout << pA->x << '\t' << pA->y << endl;
 	*/
-#ifdef DISTACE_CHECK
+#ifdef DISTANCE_CHECK
 	Point A;
 	A.SetX(2); A.SetY(3);
 	Point B;
@@ -94,12 +115,20 @@ void main()
 	Point C;
 	C.SetX(10); C.SetY(12);
 	cout << A.GetX() << "\t" << A.GetY() << "\t" << endl;
-	cout << "The disnance between A and B: " << A.Distance(B) << endl;
-	cout << "The disnance between A and C: " << A.Distance(C) << endl;
-	cout << "The disnance between B and C: " << B.Distance(C) << endl;
-	cout << "The disnance between B and C: " << Distance(A, B) << endl;
-#endif // DISTACE_CHECK
+	cout << delimiter << endl;
+	cout << "The distance between A and B: " << A.Distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "The distance between A and C: " << A.Distance(C) << endl;
+	cout << delimiter << endl;
+	cout << "The distance between B and C: " << B.Distance(C) << endl;
+	cout << delimiter << endl;
+	cout << "The distance between B and C: " << Distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "The distance between B and C: " << Distance(B, C) << endl;
+	cout << delimiter << endl;
+#endif // DISTANCE_CHECK
 
+#ifdef CONSTRUCTOR_CHECK
 	Point A; // Default constructor
 	//cout << A.GetX() << "\t" << A.GetY() << endl;
 	A.Print();
@@ -109,4 +138,24 @@ void main()
 
 	Point C = 4; // Single-Argument constructor
 	C.Print();
+
+	Point D = C; // Copy constructor
+	D.Print();
+#endif // CONSTRUCTOR_CHECK
+
+#ifdef ASSIGNMENT_CHECK_1
+	//CopyAssignment
+	Point A(2, 3); A.Print();
+	Point B = A; B.Print();//CopyConstructor
+	Point C; C = B; C.Print(); //Assignment operator  
+#endif // ASSIGNMENT_CHECK_1
+
+#ifdef ASSIGNMENT_CHECK_2
+	int a, b, c;
+	a = b = c = 0;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+#endif // ASSIGNMENT_CHECK2
+
 }
