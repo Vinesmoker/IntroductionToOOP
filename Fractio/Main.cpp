b9923605ba0f152d31ef2fe0b7afeb85daeeb5eb
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-01-30 21:27:12 +0300

    Fraction+

--- Fractio/Main.cpp
+++ Fractio/Main.cpp
@@ -32,10 +32,75 @@ public:
 		if (denominator == 0)denominator = 1;
 		this->denominator = denominator;
 	}
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		this->set_denominator(denominator);
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Fraction(int integer, int numenator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numenator;
+		this->set_denominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	//   Methods:
+	void print()const
+	{
+		if (integer)cout << integer;
+		if (numerator)
+		{
+			if (integer)cout << "(";
+			cout << numerator << "/" << denominator;
+			if (integer)cout << ")";
+		}
+		else if (integer == 0)cout << 0;
+		cout << endl;
+	}
 };
 
 void main() 
 {
 	setlocale(LC_ALL, "");
+	Fraction A;
+	A.print();
+	
+	Fraction B = 5;
+	B.print();
 
+	Fraction C(2, 3);
+	C.print();
+	
+	Fraction D(2, 3, 4);
+	D.print();
+
+	Fraction E = D;
+	E.print();
 }
