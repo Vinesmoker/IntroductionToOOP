f4a8449b7c6923cb57caf728f9e3cba75faed979
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-02-13 20:49:29 +0300

    SerpartionDone

---
+++ String/String.h
@@ -0,0 +1,47 @@
+//String.h
+#pragma once
+
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;;
+
+////////////////////////////////////////////////////////////////////////////
+///////////////// Объявление класса - CLASS DECLORATION ////////////////////
+
+class String;
+String operator+(const String& left, const String& right);
+std::ostream& operator<<(std::ostream& os, const String& obj);
+
+class String
+{
+	int size;		//размер строки в Байтах
+	char* str;		//адрес строки в динамической памяти
+public:
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+
+	//				Constructors:
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other)noexcept;
+	~String();
+
+	//				Operators:
+	String& operator=(const String& other);
+	String& operator=(String&& other)noexcept;
+
+	String& operator+=(const String& other);
+
+	const char& operator[](int i)const;
+	char& operator[](int i);
+
+	//				Methods:
+	void print()const;
+};
+
+//////////////////// CLASS DECLORATION END////////////////////
+//////////////////////////////////////////////////////////////
